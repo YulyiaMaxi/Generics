9b573ed837905cb9d7f754baf8f08d712c10129d
Netology <orjuice@mail.ru>
2023-05-25 18:49:36 +0300

    My commit

--- src/test/java/AviaSoulssTest.java
+++ src/test/java/AviaSoulssTest.java
@@ -96,6 +96,62 @@ public class AviaSoulssTest {
     }
 
     @Test
+    public void testSearch2() {
+        AviaSouls manager = new AviaSouls();
+
+        Ticket ticket1 = new Ticket("Msc", "Spb", 15000, 12, 14);
+
+        Ticket ticket2 = new Ticket("Msc", "Spb", 16500, 8, 10);
+
+        Ticket ticket3 = new Ticket("Ny", "Chi", 30000, 12, 13);
+
+        Ticket ticket4 = new Ticket("Ekb", "Spb", 9000, 10, 12);
+
+        Ticket ticket5 = new Ticket("Msc", "Spb", 15000, 1, 3);
+
+        Ticket ticket6 = new Ticket("Prs", "Spb", 50000, 12, 14);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+
+        Ticket[] actual = manager.search("Prs", "Spb");
+        Ticket[] expected = {ticket6};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearch3() {
+        AviaSouls manager = new AviaSouls();
+
+        Ticket ticket1 = new Ticket("Msc", "Spb", 15000, 12, 14);
+
+        Ticket ticket2 = new Ticket("Msc", "Spb", 16500, 8, 10);
+
+        Ticket ticket3 = new Ticket("Ny", "Chi", 30000, 12, 13);
+
+        Ticket ticket4 = new Ticket("Ekb", "Spb", 9000, 10, 12);
+
+        Ticket ticket5 = new Ticket("Msc", "Spb", 15000, 1, 3);
+
+        Ticket ticket6 = new Ticket("Prs", "Spb", 50000, 12, 14);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+
+        Ticket[] actual = manager.search("Prs", "Ekb");
+        Ticket[] expected ={}; // здесь именно пустой массив должен выйти, а не null
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void testCompareByTime() {
         AviaSouls manager = new AviaSouls();
 
